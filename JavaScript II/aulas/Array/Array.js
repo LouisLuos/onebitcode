9619c0dad2068a490587a8465c394b40d4856e4b
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-05-19 16:01:38 -0300

    primeiro commit onebitcode

---
+++ JavaScript II/aulas/Array/Array.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+//Adicionando elementos:
+// push: Adiciona um elemento no final do array e devolve o novo tamanho do array
+// unshift: Adiciona um elemento no começo do array e devolve o novo tamanho do array
+
+
+//Removendo elementos:
+// pop: Remove um elemento no final do array e devolve o elemento removido
+// shift: Remove um elemento no começo do array e devolve o elemento removido
