b58094cadd405dc24aba2b0340e2b6927f1ced21
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-05 16:59:39 -0300

    aula função anonima/ high order functions

---
+++ JavaScript II/aulas/HOF/script.js
@@ -0,0 +1,49 @@
+
+const frutas = ["Banana", "Maçã", "Pêra", "Uva", "Abacaxi"]
+
+function exibirElemento (fruta, indice, lista) {
+    console.log({
+        fruta,
+        indice,
+        lista
+    })
+}
+
+
+frutas.forEach(exibirElemento)
+
+
+
+
+
+
+
+
+
+
+
+// Essas funções que são passadas como parâmetros geralmente são chamadas de callbacks
+// Um exemplo comum de high-order function no javascript é a função .forEach() dos arrays
+function exibirElemento(elemento, indice, array) {
+  console.log({
+    elemento,
+    indice,
+    array
+  })
+}
+
+const lista = ["Maçã", "Banana", "Laranja", "Limão", "Uva"]
+// Forma tradicional
+for (let i = 0; i < lista.length; i++) {
+  exibirElemento(lista[i], i, lista)
+}
+// Forma funcional
+lista.forEach(exibirElemento)
+// Também poderia ser feito:
+lista.forEach(function (elemento, indice, array) {
+  console.log({
+    elemento,
+    indice,
+    array
+  })
+})
