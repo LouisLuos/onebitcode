489620dd1e310f9feccbc54ecb687f9883b30ccc
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-07 14:25:11 -0300

    modificações

--- JavaScript II/exercicios/sistema-vaga-de-empregro/script.js
+++ JavaScript II/exercicios/sistema-vaga-de-empregro/script.js
@@ -1,39 +1,74 @@
 const vagas = []
 
-
-
-
 function listarVagas() {
     if (vagas.length === 0){
         alert("Não há nenhuma vaga cadastrada")        
-    } else {
+        return;
+    }
 
-    let lista_de_vagas = ''
+    let lista_de_vagas = '--- Vagas Disponíveis ---\n'
 
     vagas.forEach(function (vaga, indice){
-        lista_de_vagas += `Vaga: ${indice + 1}, ${vaga.nome}, pessoas cadastradas: ${vaga.candidatos.length}`
+        lista_de_vagas += `Vaga ${indice + 1}: ${vaga.nome} (${vaga.candidatos.length} candidatos)\n`
     })
     
     alert(lista_de_vagas)
 }
-}
-
-
 
 function criar_vaga() {
     let vaga = {}
     vaga.nome = prompt("Digite o nome da vaga")
     vaga.descricao = prompt("Digite a descrição")
-    vaga.limite = prompt("Qual a data limite? (00/00/0000")
+    vaga.dataLimite = prompt("Qual a data limite? (dd/mm/aaaa)")
     vaga.candidatos = []
-    const confirmacao = prompt("Confirma a criação da vaga?").toUpperCase()
-    if (confirmacao === "SIM") {
+    
+    const confirmacao = confirm(
+        "Deseja criar a vaga com as seguintes informações?\n" +
+        "Nome: " + vaga.nome + "\n" +
+        "Descrição: " + vaga.descricao + "\n" +
+        "Data Limite: " + vaga.dataLimite
+    )
+
+    if (confirmacao) {
         vagas.push(vaga)
         alert("Vaga criada com sucesso!")
     } else {
-        alert("A vaga não foi criada")
+        alert("Operação cancelada. A vaga não foi criada.")
+    }
+}
+
+function visualizarVaga() {
+    if (vagas.length === 0) {
+        alert("Não há vagas cadastradas!")
+        return;
+    }
+    
+    const indiceUsuario = parseInt(prompt("Qual índice da vaga deseja visualizar?"))
+    const indiceArray = indiceUsuario - 1
+
+    const vagaEncontrada = vagas[indiceArray]
+
+    if (vagaEncontrada) { 
+        let nomesDosCandidatos = "Nenhum candidato inscrito."
+        if (vagaEncontrada.candidatos.length > 0) {
+            nomesDosCandidatos = vagaEncontrada.candidatos.join(", ")  
+        }
+        
+        const textoAlerta = `--- Detalhes da Vaga ---\n` +
+                  `Índice: ${indiceUsuario}\n` +
+                  `Nome: ${vagaEncontrada.nome}\n` +
+                  `Descrição: ${vagaEncontrada.descricao}\n` +
+                  `Data Limite: ${vagaEncontrada.dataLimite}\n` +
+                  `Quantidade de Candidatos: ${vagaEncontrada.candidatos.length}\n` +
+                  `Candidatos Inscritos: ${nomesDosCandidatos}`
+
+        alert(textoAlerta)
+    } else {
+        alert("Vaga não existe! Verifique o índice digitado.")
     }
 }
 
 
-
+criar_vaga()
+listarVagas()
+visualizarVaga()
