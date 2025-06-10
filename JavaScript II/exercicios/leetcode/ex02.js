23116ae1b5ff00c35e4d9046cb3762bfe9909199
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-10 14:41:29 -0300

    encontrando o maior número em um array

--- JavaScript II/exercicios/leetcode/ex02.js
+++ JavaScript II/exercicios/leetcode/ex02.js
@@ -4,8 +4,9 @@ const arr = [1, 10, 5, 3, 15, 25]
 function maiorNumero (array) {
     let maiorNumero = 0
     for (i = 0; i < array.length; i++) {
-        if (array[i] > maiorNumero) {
-            maiorNumero = array[i]
+        let valorAtual = array[i]
+        if (valorAtual > maiorNumero) {
+            maiorNumero = valorAtual
         }
     }
     return maiorNumero
