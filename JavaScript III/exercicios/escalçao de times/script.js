7cabbad1d1481ce576de702ff8056bf77c6b8f99
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-15 02:05:00 -0300

    feat; remover jogador

--- JavaScript III/exercicios/escalçao de times/script.js
+++ JavaScript III/exercicios/escalçao de times/script.js
@@ -1,4 +1,4 @@
-
+const todosJogadores = []
 
 function adicionarJogador () {
     
@@ -11,15 +11,43 @@ function adicionarJogador () {
     const ulEscalados = document.getElementById("ul-time")
     
     const newLi = document.createElement("li")
+    newLi.id = "jogador-" + numeroJogador
 
     newLi.innerHTML = `<p>Posição: ${posicaoJogador}</p> \n <p>Nome: ${nomeJogador}</p> \n <p>Número: ${numeroJogador}</p> \n`
-
+    
+    const arrayJogadores = [posicaoJogador, nomeJogador, numeroJogador]
+    todosJogadores.push(arrayJogadores)
     ulEscalados.appendChild(newLi)
     
+    
 
-    document.getElementById('input-posicao').value = ''
-    document.getElementById('input-nome').value = ''
-    document.getElementById('input-numero').value = ''
+    document.getElementById('input-posicao').value = ""
+    document.getElementById('input-nome').value = ""
+    document.getElementById('input-numero').value = ""
     }
+
+    console.log(todosJogadores)
     
 }
+
+function removerJogador () {
+    const numeroCamisa = prompt("Digite o número da camisa")
+    for (let i = 0; i < todosJogadores.length; i++) {
+        if (todosJogadores[i][2] === numeroCamisa) {
+            todosJogadores.splice(i, 1)
+
+        
+        const idParaBuscar = "jogador-" + numeroCamisa
+        const liParaRemover = document.getElementById(idParaBuscar)
+        liParaRemover.remove()
+
+        }   
+    }
+
+    
+
+
+    console.log(todosJogadores)
+}
+
+
