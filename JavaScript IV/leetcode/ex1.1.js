dde78ddc935447dd2374ec673408c5f4b65435aa
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-16 19:24:49 -0300

    fixação template literals

--- JavaScript IV/leetcode/ex1.1.js
+++ JavaScript IV/leetcode/ex1.1.js
@@ -4,3 +4,16 @@ const idade = 19
 const saudacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos`
 
 console.log(saudacao)
+
+
+const destinatario = "Douglas"
+const assunto = "Creme contra calvice"
+
+const email = `Olá, ${destinatario}
+
+Gostaríamos de informar que  recebemos o seu email sobre, ${assunto}.
+
+Atenciosamente,
+Equipe de suporte`
+
+console.log(email)
