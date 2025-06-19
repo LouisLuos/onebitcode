06e653c7a32d1aa283e9f7b6114e0e5779d44dbc
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-19 17:44:28 -0300

    exercicio arrowfunc

---
+++ JavaScript IV/arrow func/ex07.js
@@ -0,0 +1,8 @@
+//Escreva uma arrow function que receba nome e idade como parâmetros e retorne um objeto
+
+// com essas propriedades.
+
+ //Atenção à sintaxe para retornar um objeto literal de forma implícita.
+
+ const pessoa = (nome, idade) => ({nome, idade})
+ console.log(pessoa("Luiz", 19))
