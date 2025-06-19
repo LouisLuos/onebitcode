890c4839daa23e7c0e35cbb55785f84dbd39cae0
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-19 17:30:47 -0300

    exercicio arrowfunc

---
+++ JavaScript IV/arrow func/ex06.js
@@ -0,0 +1,8 @@
+//Crie uma arrow function chamada verificarIdade que receba uma idade como parâmetro.
+//  A função deve retornar a string "Maior de idade" se a idade for maior ou igual a 18,
+//  e "Menor de idade" caso contrário. Por ser uma lógica condicional, a função terá mais de uma linha.
+
+
+const verificarIdade = (idade) => idade >= 18 ? "Maior de idade" : "Menor de idade"
+
+console.log(verificarIdade(20))
