f35603c8c0daa597f285b1193b6552a9aff49058
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-19 22:05:59 -0300

    exercicios desestruturação arrays e objetos

---
+++ JavaScript IV/desestruturação arr e obj/ex11.js
@@ -0,0 +1,9 @@
+const numeros = [10, 20];
+
+const [ n1, n2 , n3 = 0 ] = numeros
+
+console.log(n1, n2, n3)
+
+// console.log(a); // Saída: 10
+// console.log(b); // Saída: 20
+// console.log(c); // Saída: 0
---
+++ JavaScript IV/desestruturação arr e obj/ex12.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+
+[a, b] = [b, a]
+
+
+
+
+console.log(a); // Saída: 10
+console.log(b); // Saída: 5
