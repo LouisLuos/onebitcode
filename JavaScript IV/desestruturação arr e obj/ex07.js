d5467d1fea016d528dabd601ae643eb2d57554a8
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-06-19 21:48:46 -0300

    exercicios desestruturação arrays e objetos

---
+++ JavaScript IV/desestruturação arr e obj/ex07.js
@@ -0,0 +1,16 @@
+const filme = {
+  nome: 'Interestelar',
+  ano: 2014,
+};
+
+let { nome, ano, genero } = filme
+genero = "Gênero não definido"
+anoLancamento = ano
+
+console.log(nome)
+console.log(anoLancamento)
+console.log(genero)
+
+// console.log(nome);             // Saída: Interestelar
+// console.log(anoLancamento);    // Saída: 2014
+// console.log(genero);           // Saída: Gênero não definido
---
+++ JavaScript IV/desestruturação arr e obj/ex08.js
@@ -0,0 +1,6 @@
+const frutas = ['Banana', 'Maçã', 'Laranja', 'Uva'];
+
+const [fruta0, fruta1] = frutas
+
+ console.log(fruta0); // Saída: Banana
+ console.log(fruta1);  // Saída: Maçã
