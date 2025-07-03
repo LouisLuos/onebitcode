8bd469a654bfb974539c980a93eeb9c2ac4bfe9f
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-07-03 17:00:37 -0300

    aprendendo a utilizar npm

--- JavaScript IV/npm/index.js
+++ JavaScript IV/npm/index.js
@@ -2,3 +2,9 @@ const lodash = require("lodash")
 
 console.log(lodash.isArray([0, 1, 2]))
 console.log(lodash.kebabCase("Curupira Mula sem cabeça"))
+
+
+//npm install (nome do pacote)
+//npm uninstall (nome do pacote)
+//npm install --global (nome do pacote) instala na sua maquina
+
