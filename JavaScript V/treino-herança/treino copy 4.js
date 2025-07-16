48a09755d546295bc29c713e186459d7d7dcf902
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-07-16 15:52:27 -0300

    exercicio 4 herança

--- JavaScript V/treino-herança/treino copy 4.js
+++ JavaScript V/treino-herança/treino copy 4.js
@@ -1,20 +1,25 @@
-class Author {
-    constructor (name, bio) {
-        this.name = name
-        this.bio = bio
-    }
+class Account {
+  constructor(initialBalance) {
+    this.balance = initialBalance
+  }
 }
 
-class Post {
-    constructor (tittle, content, author) {
-        this.tittle = tittle
-        this.content = content
-        this.author = author
-    }
+class SavingsAccount extends Account {
+  applyInterest(interestRate) {
+    this.balance += this.balance * interestRate
+  }
 }
 
-const autor1 = new Author ("Luiz", "Programador Back-end")
-const post1 = new Post ("Aprendendo JS", "POO", autor1)
-console.log(`O post ${post1.tittle} foi escrito por ${post1.author.name}`)
+class CheckingAccount extends Account {
+  deductFee(fee) {
+    this.balance -= fee
+  }
+}
 
-console.log(Post)
+const savings = new SavingsAccount(1000)
+savings.applyInterest(0.05)
+console.log(`Saldo poupança: ${savings.balance}`)
+
+const checking = new CheckingAccount(500)
+checking.deductFee(5); // 5 de taxa
+console.log(`Saldo corrente: ${checking.balance}`)
