512ba383b9116fd794ad9ea5856325246271e910
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-07-22 22:11:01 -0300

    treinando metodos arrays

--- JavaScript II/exercicios/treino/script copy 3.js
+++ JavaScript II/exercicios/treino/script copy 3.js
@@ -1,5 +1,8 @@
-const numeros = [10, 25, 30, 5, 12]
+const produtos = [{ nome: 'Camiseta', preco: 80 }, { nome: 'Calça Jeans', preco: 220 }, { nome: 'Tênis', preco: 350 }];
 
-const dobro = numeros.map(index => index * 2)
-console.log(numeros)
-console.log(dobro)
+const produtosDesconto = produtos.map(function (objeto) {
+    objeto.ComDesconto = objeto.preco * 0.9
+    return objeto
+})
+
+console.log(produtosDesconto)
