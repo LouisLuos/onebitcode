db79e2f8197936f50b646902230d3855742d03bc
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-07-22 22:15:47 -0300

    treinando metodos arrays

--- JavaScript II/exercicios/treino/script copy 2.js
+++ JavaScript II/exercicios/treino/script copy 2.js
@@ -1,5 +1,5 @@
-const numeros = [10, 25, 30, 5, 12]
+const tempsCelsius = [0, 15, 22, 30, -5]
 
-const dobro = numeros.map(index => index * 2)
-console.log(numeros)
-console.log(dobro)
+const tempsFahrenheit = tempsCelsius.map(index => index = index * 9/5 + 32)
+
+console.log(tempsFahrenheit)
