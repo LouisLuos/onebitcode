818bcfe28c63e87958c18d3ed1edfe1529cdeaca
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-08-25 14:53:13 -0300

    primeiro projetinho em type

---
+++ Typescript/primeiro-projeto/src/index.ts
@@ -0,0 +1,37 @@
+function sendSpaceship(name: string, capitain: string) {
+    const spaceship = {
+        name,
+        capitain,
+        velocity: 20,
+        inMission: true,
+        crew: []   
+    }
+    
+    alert(`A nave ${spaceship.name} com os tripulantes ${spaceship.crew.forEach((pessoa) => console.log(pessoa))} foram enviados a uma missão a bordo do capitão ${spaceship.capitain}`)
+    return spaceship
+}
+
+function accelerate(acelerar: number, spaceship: {name: string, velocity: number}){
+    if(spaceship.velocity > acelerar){
+        alert(`reduzindo a velocidade de ${spaceship.velocity} para ${acelerar}`)
+        spaceship.velocity = acelerar
+    } else {
+        alert(`aumento a velocidade da nave para ${acelerar}`)
+        spaceship.velocity = acelerar
+    }
+    return spaceship
+}
+
+function addTripulante(tripulante: string, spaceship: {crew: string[]}){
+    spaceship.crew.push(tripulante)
+    alert("O triuplante" + tripulante + "Foi adicionado a Naveeeeeeee")
+    return spaceship
+}
+
+const nomeDaNave = prompt("Digite o nome da nave")
+const nomeDoCapitao = prompt("Digite o nome do capitão")
+const primeiroTripulante = prompt("Digite o nome do tripulante")
+const espacoNave = sendSpaceship(nomeDaNave, nomeDoCapitao)
+
+console.log(accelerate(100, espacoNave))
+console.log(addTripulante(primeiroTripulante, espacoNave))
