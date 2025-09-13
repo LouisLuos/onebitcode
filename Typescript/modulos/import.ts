49a7d256d3a6f1d8390359b055bc70b13aec7233
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-09-13 14:29:07 -0300

    utilizando modulos no typescript

---
+++ Typescript/modulos/import.ts
@@ -0,0 +1,15 @@
+import { Spaceship } from './export'
+import * as lodash from 'lodash'
+interface AttackSpaceship extends Spaceship {
+  weapons: number
+}
+
+let xwing: AttackSpaceship = {
+  name: 'X-Wing',
+  pilot: 'Luke Skywalker',
+  speed: 50,
+  weapons: 4
+}
+
+
+console.log(lodash.camelCase(xwing.name))
