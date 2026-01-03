30480f3009e32f1e20040724d46ce18393e04180
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-01-03 17:13:20 -0300

    aprendendo boostrap

--- bootstrap-sass/boostrap-webpack/webpack.config.js
+++ bootstrap-sass/boostrap-webpack/webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const autoprefixer = require('autoprefixer')
 module.exports = {
   mode: 'development',
   entry: './src/js/main.js',
@@ -17,5 +17,48 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                // Optional: Silence Sass deprecation warnings. See note below.
+                silenceDeprecations: [
+                  'mixed-decls',
+                  'color-functions',
+                  'global-builtin',
+                  'import'
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
 }
