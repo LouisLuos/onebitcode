b30adb2b9fc9ce600c4b4c27492e59f875dda38f
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-01-20 16:13:28 -0300

    resolevndo primeiro exercicio react

---
+++ frontend/react/03-react-com-vite/first-react-projetct/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>Olá mundo!</h1>
+      <ul>
+        <li>Vou ficar muito cabeça</li>
+        <li>Meu planejamento inclui estudo</li>
+        <li>Além de estudo preciso de saude mental</li>
+        <li>Todos esses projetos que faço me ajudam a me aperfeiçoar</li>
+        <li>Quero ser um bom dev e resolver problemas do mundo real</li>
+        <li>Vai chegar o momento, constância é a chave para o aprendizado!</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
