d3f66bed3b80f142761f08038fcda233015802c2
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-01-20 15:32:38 -0300

    primeiro projeto react com vite

---
+++ frontend/react/01-primeira-pag/index.jsx
@@ -0,0 +1,16 @@
+const element = <h1>Olá mundo!</h1>
+
+const paragrafo = <p>Teste do paragrafo</p>
+
+const juntarTudo = (
+    <div>
+        <h1></h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+)
---
+++ frontend/react/01-primeira-pag/index2.jsx
@@ -0,0 +1,13 @@
+function MeuComponente() {
+    return (<h1>Header do componente</h1>) 
+}
+
+function retornarVariasCoisas(){
+    return (
+        <div className="divPai">
+            <p>COnteudo</p>
+            <p>Another content</p>
+            <button id="confirma">Enviar</button>
+        </div>
+    )
+}
