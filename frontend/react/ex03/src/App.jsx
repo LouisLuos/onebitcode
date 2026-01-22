12fcadd20d9bcbc12a1d95f02a963ee121aa5cb1
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-01-21 22:53:09 -0300

    finalizando exercicio 03

--- frontend/react/ex03/src/App.jsx
+++ frontend/react/ex03/src/App.jsx
@@ -1,10 +1,16 @@
 import CardProfile from "./components/Card"
 
-
 export default function App() {
   return (
     <>
-    <CardProfile></CardProfile>
+      <CardProfile 
+        imgProfile="https://img.freepik.com/fotos-gratis/retrato-de-homem-branco-isolado_53876-40306.jpg?w=740"
+        tittleName="Luiz Henrique"
+        descriptonCard="FullStack JavaScript Developer"
+        phone="81988474251"
+        email="luiz.contato@gmail.com"
+      />
+      
     </>
   )
 }
--- frontend/react/ex03/src/components/Card.jsx
+++ frontend/react/ex03/src/components/Card.jsx
@@ -1,9 +1,22 @@
 import styles from "./styleContainer.module.css"
 
-function Card() {
+// Mantive exatamente os seus nomes de props
+function Card({imgProfile, tittleName, descriptonCard, phone, email}) {
     return (
         <div className={styles.container}>
-            <img className="imgProfile" src="" alt="" />
+            {/* Usando a variável imgProfile no src */}
+            <img className={styles.imgProfile} src={imgProfile} alt="" />
+            
+            {/* Usando as chaves {} para o texto aparecer */}
+            <h1 className={styles.tittleName}>{tittleName}</h1>
+            
+            <p className={styles.descriptonCard}>{descriptonCard}</p>
+            <p className={styles.descriptonCard}>{phone}</p>
+            <p className={styles.descriptonCard}>{email}</p>
+            
+            <button className={styles.buttonCard}>Github</button>
+            <button className={styles.buttonCard}>Linkedin</button>
+            <button className={styles.buttonCard}>Portifólio</button>
         </div>
     )
 }
