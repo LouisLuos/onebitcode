0da70fc2af8c2ec9a1515b6f74eeffe8ccca0a6e
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-02-19 15:12:57 -0300

    retornando aqrquivos estaticos com express

---
+++ backend/node-web/03-retornando-arquivos/public/assets/script.js
@@ -0,0 +1,4 @@
+document.getElementById("teste").addEventListener('click', () => {
+    alert("Você clicou no botão")
+    console.log("clickou")
+})
---
+++ backend/node-web/03-retornando-arquivos/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+const path = require('path');
+const PORT = 3000
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')    
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`)
+})
