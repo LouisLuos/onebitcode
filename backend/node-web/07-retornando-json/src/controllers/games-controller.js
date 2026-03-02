d8500120e8ea5040343d5e18c340c63fc1edcb79
Luiz Henrique <luiz.contato.1000@gmail.com>
2026-03-02 19:59:19 -0300

    aprendendo rotas get api rest

---
+++ backend/node-web/07-retornando-json/src/controllers/games-controller.js
@@ -0,0 +1,39 @@
+const games = [
+  { id: 1, name: 'Legend of Mana', genres: ['action-rpg'], year: 1999 },
+  { id: 2, name: 'World of Warcraft', genres: ['mmorpg'], year: 2004 },
+  { id: 3, name: 'Metal Gear Solid', genres: ['stealth', 'action-adventure'], year: 1998 },
+  { id: 4, name: 'Sonic Adventure 2', genres: ['platformer'], year: 2001 },
+  { id: 5, name: 'Age of Empires 2', genres: ['real-time-strategy'], year: 1999 }
+]
+
+module.exports = {
+
+    // Esquema de rotas que o frontend consegue manipular a partir desse formato que estamos criando
+
+
+    // GET /games
+    index: (req, res) => {
+      res.json(games)
+    },
+
+    // GET /games/:id
+    show: (req, res) => {
+        console.log(req.params)
+        const { id } = req.params
+        const game = games.find(game => game.id === Number(id))
+        if (!game) {
+            res.status(404)
+            res.json({ message: 'Game not found' })
+        } else {
+            res.status(200)
+            res.json(game)
+        }
+    }
+
+    // POST /games
+
+    // PUT /games/:id
+
+    // DELETE /games/:id
+  
+}
